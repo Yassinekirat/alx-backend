fd9cd478b38357c521ca7c8ebce328243841edb0
YASSINE EL KIRAT <yassine.elkirat@gmail.com>
2024-08-19 11:18:16 +0100

    checkin again

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -3,8 +3,8 @@
 from typing import Tuple
 
 
-def index_range(page: int, page_size: int) -> tuple[int, int]:
-    """Simple helper function"""
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """Simple helper function to calculate start and end indices"""
     start = (page - 1) * page_size
     end = start + page_size
     return (start, end)
