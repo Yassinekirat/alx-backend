413c36628678391fe899e6c70ab1ae1d330e399d
YASSINE EL KIRAT <yassine.elkirat@gmail.com>
2024-08-19 11:13:20 +0100

    task 0

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,1 +1,10 @@
 #!/usr/bin/env python3
+"""Simple helper function"""
+from typing import Tuple
+
+
+def index_range(page: int, page_size: int) -> tuple[int, int]:
+    """Simple helper function"""
+    start = (page - 1) * page_size
+    end = start + page_size
+    return (start, end)
