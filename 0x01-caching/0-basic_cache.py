45840e11483fcd8582c92ea9855b00bd6894fa3c
YASSINE EL KIRAT <yassine.elkirat@gmail.com>
2024-08-21 10:27:21 +0100

    adding main files and testing task 0

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,1 +1,17 @@
 #!/usr/bin/env python3
+"""Basic dictionary"""
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    def put(self, key, item):
+        """Basic dictionary"""
+        if key is None or item is None:
+            return
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """Basic dictionary"""
+        if key is None:
+            return None
+        return self.cache_data.get(key)
