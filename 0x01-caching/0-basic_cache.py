dd956002d848d5abcea67bb096e8ac50e687bf5b
YASSINE EL KIRAT <yassine.elkirat@gmail.com>
2024-08-21 10:39:28 +0100

    task 0

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -4,14 +4,18 @@ from base_caching import BaseCaching
 
 
 class BasicCache(BaseCaching):
+    """A basic caching system with no limit"""
+
     def put(self, key, item):
-        """Basic dictionary"""
+        """Add an item in the cache.
+        """
         if key is None or item is None:
             return
         self.cache_data[key] = item
 
     def get(self, key):
-        """Basic dictionary"""
+        """Retrieve an item from the cache by key.
+        """
         if key is None:
             return None
         return self.cache_data.get(key)
