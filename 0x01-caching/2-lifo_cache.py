9d7ca8a2b30101612a5869e2268f26cccd929c95
YASSINE EL KIRAT <yassine.elkirat@gmail.com>
2024-08-21 11:24:40 +0100

    task 2

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -1,1 +1,29 @@
 #!/usr/bin/env python3
+"""LIFO Caching system"""
+from base_caching import BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    def __init__(self):
+        """Initialize the cache"""
+        super().__init__()
+        self.order = []
+
+    def put(self, key, item):
+        """Add an item to the cache with LIFO eviction policy."""
+        if key is None or item is None:
+            return
+        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            last_cache = self.order.pop(-1)
+            del self.cache_data[last_cache]
+            print(f"DISCARD: {last_cache}")
+
+        self.cache_data[key] = item
+        self.order.append(key)
+
+    def get(self, key):
+        """Retrieve an item from the cache by key
+        """
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data.get(key)
