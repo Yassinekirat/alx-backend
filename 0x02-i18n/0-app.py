c100f5969a71560f8211523d42c67d9942c58595
YASSINE EL KIRAT <yassine.elkirat@gmail.com>
2024-08-27 15:57:06 +0100

    fixing pycodestyle

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -4,9 +4,10 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
 
+@app.route('/')
 def index():
+    """Basic Flask app"""
     return render_template('index.html')
 
 
