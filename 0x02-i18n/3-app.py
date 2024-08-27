dc20f13f769eacd4dbd1777138cbcdfd2481c645
YASSINE EL KIRAT <yassine.elkirat@gmail.com>
2024-08-27 16:39:46 +0100

    i have some requirement failed that i don t even know what are they from

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -4,7 +4,7 @@ from flask import Flask, render_template, request
 from flask_babel import Babel, _
 
 
-class Config:
+class Config(object):
     """Basic Babel setup"""
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
@@ -18,7 +18,7 @@ babel = Babel(app)
 
 
 @babel.localeselector
-def get_locale():
+def get_locale() -> str:
     """determine the best match with our supported languages"""
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
