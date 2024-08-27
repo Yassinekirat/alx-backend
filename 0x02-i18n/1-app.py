51e7b11cfbaa8c26ea6dc07f36cf9d80b28e8f63
YASSINE EL KIRAT <yassine.elkirat@gmail.com>
2024-08-27 15:54:51 +0100

    lets check

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,1 +1,24 @@
 #!/usr/bin/env python3
+"""Basic Babel setup"""
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+class Config:
+    """Basic Babel setup"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+babel = Babel(app)
+
+@app.route('/')
+def index():
+    """Basic Babel setup"""
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
