7cda40dda88f1482f0c9009371657ff20a37b162
YASSINE EL KIRAT <yassine.elkirat@gmail.com>
2024-08-28 01:34:01 +0100

    task 3

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
-"""Basic Babel setup"""
+"""Basic Flask app"""
 from flask import Flask, render_template, request
-from flask_babel import Babel, _
+from flask_babel import Babel
 
 
 class Config():
-    """Basic Babel setup"""
+    """Configuration class for the Flask app"""
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
@@ -16,6 +16,13 @@ app.config.from_object(Config)
 babel = Babel(app)
 
 
+@app.route('/')
+def index():
+    """define a route and render HTML template"""
+    locale = get_locale()
+    return render_template('3-index.html', locale=locale)
+
+
 @babel.localeselector
 def get_locale():
     """locale from request: best language match"""
@@ -23,13 +30,6 @@ def get_locale():
     if locale in app.config['LANGUAGES']:
         return locale
     return request.accept_languages.best_match(app.config['LANGUAGES'])
-
-
-@app.route('/')
-def index() -> str:
-    """Basic Babel setup"""
-    locale = get_locale()
-    return render_template('3-index.html', locale=locale)
 
 
 if __name__ == '__main__':
